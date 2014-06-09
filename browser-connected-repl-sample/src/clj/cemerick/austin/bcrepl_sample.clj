5ad86f90876540f763cf30a2aa3b09be02e42dbf
Matt Kocubinski <mkocubinski@gmail.com>
2014-06-09 00:05:25 -0400

    Yea.

--- browser-connected-repl-sample/src/clj/cemerick/austin/bcrepl_sample.clj
+++ browser-connected-repl-sample/src/clj/cemerick/austin/bcrepl_sample.clj
@@ -17,8 +17,13 @@
   (GET "/*" req (page)))
 
 (defn run
-  []
-  (defonce ^:private server
+  [] (defonce ^:private server
     (ring.adapter.jetty/run-jetty #'site {:port 8080 :join? false}))
   server)
 
+(defn cljs-browser-repl
+  "Fire up a browser-connected ClojureScript REPL"
+  []
+  (let [repl-env (reset! cemerick.austin.repls/browser-repl-env
+                         (cemerick.austin/repl-env))]
+    (cemerick.austin.repls/cljs-repl repl-env)))
